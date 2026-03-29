c4e3d0270612798a9931dc59f368bf89b4fce976
Albion Berisha <57451316+Albionn1@users.noreply.github.com>
2026-03-29 20:32:51 +0200

    Changed to Fusion style windows

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     a.setWindowIcon(QIcon(":/images/images/icon.png"));
 
+    a.setStyle("Fusion");
     WelcomeWindow welcome;
     if (welcome.exec() == QDialog::Accepted) {
         MainWindow w;
